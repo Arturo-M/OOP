6dc9cd12a0c47f85879fc5099d04c26382214a26
Arturo Mercado <arturo28m@gmail.com>
2019-09-29 14:41:12 -0700

    Update Program.cs

--- pelicula/Program.cs
+++ pelicula/Program.cs
@@ -14,19 +14,19 @@ namespace pelicula
     {
         static void Main(string[] args)
         {
-            Peli Pe1 = new Peli();
-            Pe1.Titulo = "El gigante de hierro";
-            Pe1.Año = 1999;
-            Pe1.Pais = "Estados Unidos";
-            Pe1.Direct = "Brad Bird";
-            Console.WriteLine("Titulo: {0} \nAño: {1} \nPais: {2} \nDirector: {3}", Pe1.Titulo,Pe1.Año,Pe1.Pais,Pe1.Direct);
+            Peli P1 = new Peli();
+            P1.Titulo = "El gigante de hierro";
+            P1.Año = 1999;
+            P1.Pais = "Estados Unidos";
+            P1.Direct = "Brad Bird";
+            Console.WriteLine("Titulo: {0} \nAño: {1} \nPais: {2} \nDirector: {3}", P1.Titulo,P1.Año,P1.Pais,P1.Direct);
           
-            Peli Pe2 = new Peli();
-            Pe2.Titulo = "El extraño mundo de Jack";
-            Pe2.Año = 1993;
-            Pe2.Pais = "Estados unidos";
-            Pe2.Direct = "Henry Selick";
-            Console.WriteLine("\nTitulo: {0} \nAno :{1} \nPais: {2} \nDirectores: {3}", Pe2.Titulo, Pe2.Año,Pe2.Pais,Pe2.Direct);
+            Peli P2 = new Peli();
+            P2.Titulo = "El extraño mundo de Jack";
+            P2.Año = 1993;
+            P2.Pais = "Estados unidos";
+            P2.Direct = "Henry Selick";
+            Console.WriteLine("\nTitulo: {0} \nAno :{1} \nPais: {2} \nDirectores: {3}", P2.Titulo, P2.Año,P2.Pais,P2.Direct);
         }
     }
 }
